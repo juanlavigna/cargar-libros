5b7e80bcf22dc8908a96d9c9e7682c31f3ddb391
Juan Lavigna <juanlavigna@gmail.com>
2022-08-01 15:15:32 -0300

    Declare VARs with DOM information

---
+++ script.js
@@ -0,0 +1,9 @@
+// DIVS
+let confirmaciones = document.querySelector("confirmaciones");
+let botonCargar = document.querySelector("#cargar");
+let botonRefrescar = document.querySelector("#refrescar");
+let listaDesordeada = document.querySelector("ul");
+// INPUTS
+let inputNombre = document.querySelector("#nombre");
+let inputAutor = document.querySelector("#autor");
+let inputDireccion = document.querySelector("#direccion");
